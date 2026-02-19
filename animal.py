a2ae9714f9148dfa040064919e66ca96acc350aa
pedrsoares <pedrohenriquemahados2009@gmail.com>
2026-02-19 10:00:29 -0300

    atividade animal

---
+++ animal.py
@@ -0,0 +1,10 @@
+Nome = "Cachorro" # variável de texto
+idade = "6" #variável int
+peso = "5" #variável int
+sexo = 'masculino' # variável de texto
+
+
+print (Nome)
+print (idade)
+print (peso)
+print (sexo)
