95b834820c85ed98512c5df3ad519c46d87b0e67
pedrsoares <pedrohenriquemachados2009@gmail.com>
2026-02-20 09:47:19 -0300

    alua aluno

---
+++ matematica.py
@@ -0,0 +1,15 @@
+valor_inteiro =int(input("digite o primeiro valor:"))
+valor_inteiro2 =int(input("digite o segumdo valor:"))
+
+
+
+
+soma = valor_inteiro + valor_inteiro2
+subtracao = valor_inteiro - valor_inteiro2
+divisao = valor_inteiro / valor_inteiro2
+multiplicacao = valor_inteiro * valor_inteiro2
+
+print ("o valor da soma e" , soma)
+print ("o valor da subtracao e" , subtracao)
+print ("o valor da divisao e divisao" , divisao)
+print ("o valor da multiplicacao" , multiplicacao)
