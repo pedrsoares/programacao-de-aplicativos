181914526b36bb3c89d44683b90f83256fc9cec1
pedrsoares <pedrohenriquemachados2009@gmail.com>
2026-02-24 11:03:36 -0300

    aulaaa

---
+++ exercicio03.py
@@ -0,0 +1,6 @@
+dolar =5
+quantos_reais =float(input("quantos reais voce tem"))
+
+divisao = quantos_reais / dolar
+print ( "voce poderar comprar dolar:" , divisao )
+
---
+++ exercio02.py
@@ -0,0 +1,6 @@
+ano_nascimento =int(input("digite o ano que vc nasceu"))
+ano_atual =int(input("digite o ano de agora"))
+
+subtracao = ano_nascimento - ano_atual
+print ("voce tem" , subtracao )
+
