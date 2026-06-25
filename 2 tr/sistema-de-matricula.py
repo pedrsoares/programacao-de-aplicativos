b9a92cefd2bdcb57757e4a303bd4a2603f9ae01f
pedrsoares <pedrohenriquemachados2009@gmail.com>
2026-06-25 10:14:22 -0300

    2506

--- 2 tr/sistema-de-matricula.py
+++ 2 tr/sistema-de-matricula.py
@@ -20,8 +20,7 @@ def cadrastro_aluno():
         "Idade": int(input("idade: ")),
         "cpf": int(input("cpf: ")),
     }
-
-  alunos.append(novo_aluno)
+alunos.append(novo_aluno)
 
     with open(BANCO_DADOS, 'w', encoding='utf-8') as f:
         json.dump(alunos, f, indent=4, ensure_ascii=False)
